8cb59d48c3c4a70b2fcb93b753d73dcd058a8da4
Andreas Daskalopoulos <Andreas.Daskalopoulos@citrix.com>
2018-06-04 15:00:34 +0300

    First commit

---
+++ app.js
@@ -0,0 +1,29 @@
+// DOM Load Event
+document.addEventListener('DOMContentLoaded', showChart);
+
+function showChart() {
+    console.log('Show chart')
+    var myChart = Highcharts.chart('container', {
+        chart: {
+            type: 'bar'
+        },
+        title: {
+            text: 'Fruit Consumption'
+        },
+        xAxis: {
+            categories: ['Apples', 'Bananas', 'Oranges']
+        },
+        yAxis: {
+            title: {
+                text: 'Fruit eaten'
+            }
+        },
+        series: [{
+            name: 'Jane',
+            data: [1, 0, 4]
+        }, {
+            name: 'John',
+            data: [5, 7, 3]
+        }]
+    });
+}
